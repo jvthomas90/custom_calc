7376e1087ca73da8c2f6cfe6254f7675d3c152a7
jvthomas90 <69699907+jvthomas90@users.noreply.github.com>
2020-09-04 01:20:29 -0500

    Better optics + minor update in logic
    Made stuff prettier! Plus I added an error handling exception in case user inputs a velocity value that exceeds the speed of light

--- CustomCalculator.py
+++ CustomCalculator.py
@@ -1,13 +1,29 @@
+print('''▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+█░▄▄▀█░▄▄█▄░▄█░▄▄▀█▀▄▄▀█▀▄▄▀█░████░██░█░▄▄██▄██▀▄▀█░▄▄
+█░▀▀░█▄▄▀██░██░▀▀▄█░██░█░▀▀░█░▄▄░█░▀▀░█▄▄▀██░▄█░█▀█▄▄▀
+█░██░█▄▄▄██▄██▄█▄▄██▄▄██░████▄██▄█▀▀▀▄█▄▄▄█▄▄▄██▄██▄▄▄
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+██░▄▄▀█░▄▄▀█░██▀▄▀█░██░█░██░▄▄▀█▄░▄█▀▄▄▀█░▄▄▀█░█░█░
+██░████░▀▀░█░██░█▀█░██░█░██░▀▀░██░██░██░█░▀▀▄█▄█▄█▄
+██░▀▀▄█▄██▄█▄▄██▄███▄▄▄█▄▄█▄██▄██▄███▄▄██▄█▄▄█▀█▀█▀
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+''')
+
 # E=mC^2
 cSpeed = 299792458 # Distance (in meters) light travels in a vacuum, in a second, per-second i.e. expressed in m/s/s or m/(s^2) notation.
 mass = float(input("Enter mass in kg (kilograms) >>> "))
 energy = mass*(cSpeed**2)  # Einstein's mass-energy equivalence formula
 print("e = m(c^2) therefore your object has %d J (joules of energy)" % energy) # Check Energy units
 
+print()
 # space-time fluidity, according to the Einstein's theories of relativity
+print()
 
-percent_of_c = float(input("Enter a number between 0.000001 and 99.99999999999999: "))
-travel_distance =  float(input("Enter a distance in light years: "))
+percent_of_c = float(input("Non-FTL 'warpspeed' (enter a value between 0.000001 and 99.99999999999999): "))
+if percent_of_c >= 100:
+    raise ValueError("Where'd you get your dilithium crystals at fam? Hook me up! Till then, I can't calculate speeds faster than light!!! #SorryNotSorry")
+travel_distance =  float(input("Enter distance to destination in light years: "))
 
 import math
 # Lorentz/length contraction formula
