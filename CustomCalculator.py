8d8521dd905f0b1e33987911074fbe36b21cf702
jvthomas90 <69699907+jvthomas90@users.noreply.github.com>
2020-09-04 02:50:22 -0500

    Update CustomCalculator.py
    This commit is all about comments. Not just in code-documentation, but also in the form of helpful suggestions for the user when it comes to the space-time dilation calculator's prompt for distance traveled. Having concrete examples seemed better than asking for an  arbitrary number as input.

--- CustomCalculator.py
+++ CustomCalculator.py
@@ -10,6 +10,7 @@ def welcome():
 ██░▀▀▄█▄██▄█▄▄██▄███▄▄▄█▄▄█▄██▄██▄███▄▄██▄█▄▄█▀█▀█▀
 ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n\n\n''')
 
+# Decide which calculation formula to work with
 def chooose():
     while True:
         choice = input("\nSelect one: 'emc2' or 'spacetime' >> ")
@@ -24,18 +25,33 @@ def chooose():
         else:
             print("\n🅧  Invalid input :(\n")
 
-def calc_emc2(): # E = m*(C^2)
+# E = m*(C^2)
+def calc_emc2():
     cSpeed = 299792458 # Distance (in meters) light travels in a vacuum, in a second, per-second i.e. expressed in m/s/s or m/(s^2) notation.
     mass = float(input("Enter mass in kg (kilograms) >>> "))
     energy = mass*(cSpeed**2)  # Einstein's mass-energy equivalence formula
-    print("\nSince e = m(c^2) your object has %d J (joules of energy)" % energy) # Check Energy units
+    print("\nSince e = m(c^2) your object has %d J (joules of energy)" % energy)
+
 
 # space-time fluidity, according to the Einstein's theories of relativity
 def calc_spacetime():
     percent_of_c = float(input("Non-FTL 'warpspeed' (enter a value between 0.000001 and 99.99999999999999): "))
     if percent_of_c >= 100:
         raise ValueError("Where'd you get your dilithium crystals at fam? Hook me up! Till then, I can't calculate speeds faster than light!!! #SorryNotSorry")
-    travel_distance =  float(input("\nEnter distance to destination in light years: "))
+    travel_distance =  float(input('''
+How far are you willing to travel at that sub-light speed? For reference: 
+    
+    - The distance from Earth to Alpha and Proxima Centauri is 4.37 light years, or 41,343,000,000,000 kilometres (25,690,000,000,000 miles). Even our nearest stellar neighbour is very far away…
+
+    - Or the red giant Betelguse, one of the largest stars that can be seen with the naked eye (located on the "belt" of the constellation of stars known as "Orion") is 640 light years away...
+
+    - The centre of the Milky Way: Our galaxy, the Milky Way, is about 100,000 light years across. The Sun lies on the Sagittarius arm and is about 26,000 light years from the centre, which very likely contains a supermassive black hole (you know, if you ever feel like visiting *shrug*)
+
+    - Anderomeda is simultaneously our nearest galactic neighbor as well as the furthest object that most people can see with the naked eye. This galaxy twice the size of the Milky Way is 2,500,000 light years away!
+    
+    - The furthest known galaxy (as of March 2016) goes by the name of GN-z11. It’s around 13.4 billion light years away (13,400,000,000 light years).
+    
+Bearing the above in mind, please enter the distance to your destination in light-years >> '''))
 
     import math
     # Lorentz/length contraction formula
@@ -48,15 +64,18 @@ def calc_spacetime():
     print(f"The journey time, as viewed from earth, is {time_measured_fromEarth} years")
     print(f"The journey time, as viewed from the spaceship is {time_measured_fromCraft} years")
 
+#prompt user to rerun program from choose() function's decision tree logic
 def again():
     calc_again = input("\nDo you want to calculate again?\nPlease type Y for YES or N for NO. >> ")
     if calc_again.upper() == 'Y':
         chooose()
     elif calc_again.upper() == 'N':
+        print("\nFarewell and godspeed, interstellar voyager!!!\n")
         exit
     else:
         again()
 
+#main function calls
 welcome()
 chooose()
 again()
